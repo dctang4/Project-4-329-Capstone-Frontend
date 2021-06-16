0205cc6322b05f6be25ad5005a82867bcde95261
Chun Hin D. Tang <dtang955472@gmail.com>
2021-06-16 13:46:39 -0400

    home page and menu page done

--- fruityapp/src/components/pages/Home.js
+++ fruityapp/src/components/pages/Home.js
@@ -1,8 +1,94 @@
-import React from 'react'
-import './Home.scss'
+import React from "react";
+import "./Home.scss";
 
 const Home = (props) => {
-  return <h1>Home Page</h1>
-}
+  return (
+    <div className="home">
+      <h1>FRUITY</h1>
+      <img
+        src="https://res.cloudinary.com/dvxvez8mj/image/upload/v1623854525/Project%204/unnamed_bi7adn.png"
+        alt="fruity drinks"
+      />
+      <h2>Top Drinks</h2>
+      <div className="top-drinks">
+        <img
+          src="https://res.cloudinary.com/dvxvez8mj/image/upload/v1623855217/Project%204/o_ueywdq.jpg"
+          alt="fruity drinks"
+        />
+        <img
+          src="https://res.cloudinary.com/dvxvez8mj/image/upload/v1623855217/Project%204/o_3_phk48b.jpg"
+          alt="fruity drinks"
+        />
+        <img
+          src="https://res.cloudinary.com/dvxvez8mj/image/upload/c_crop,h_1000,q_100,y_168/v1623857687/Project%204/o_2_tz69vx.jpg"
+          alt="fruity drinks"
+        />
+        <img
+          src="https://res.cloudinary.com/dvxvez8mj/image/upload/v1623855217/Project%204/o_1_bl5qad.jpg"
+          alt="fruity drinks"
+        />
+      </div>
+      <h2>Locations</h2>
+      <div className="locations">
+        <div className="location">
+          <h3>Location 1</h3>
+          <p>250 Granite St.</p>
+          <p>Braintree, MA 02184</p>
+          <p>(781) 308-4362</p>
+          <br/>
+          <h3>Hours</h3>
+          <div className="hours">
+            <ul>
+              <li>Monday</li>
+              <li>Tuesday</li>
+              <li>Wednesday</li>
+              <li>Thursday</li>
+              <li>Friday</li>
+              <li>Saturday</li>
+              <li>Sunday</li>
+            </ul>
+            <ul>
+              <li>11AM - 7PM</li>
+              <li>11AM - 7PM</li>
+              <li>11AM - 7PM</li>
+              <li>11AM - 7PM</li>
+              <li>11AM - 8PM</li>
+              <li>11AM - 8PM</li>
+              <li>12pm - 6PM</li>
+            </ul>
+          </div>
+        </div>
+        <div className="location">
+          <h3>Location 2</h3>
+          <p>77 Harvard Ave</p>
+          <p>Allston, MA 02134</p>
+          <p>(617) 208-8848</p>
+          <br/>
+          <h3>Hours</h3>
+          <div className="hours">
+            <ul>
+              <li>Monday</li>
+              <li>Tuesday</li>
+              <li>Wednesday</li>
+              <li>Thursday</li>
+              <li>Friday</li>
+              <li>Saturday</li>
+              <li>Sunday</li>
+            </ul>
+            <ul>
+              <li>11AM - 9PM</li>
+              <li>11AM - 9PM</li>
+              <li>11AM - 9PM</li>
+              <li>11AM - 9PM</li>
+              <li>11AM - 9PM</li>
+              <li>11AM - 9PM</li>
+              <li>11AM - 9PM</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- fruityapp/src/components/pages/Menu.js
+++ fruityapp/src/components/pages/Menu.js
@@ -1,8 +1,131 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './Menu.scss'
 
 const Menu = (props) => {
-  return <h1>Menu Page</h1>
+  const {fruityData} = props
+
+  const loaded = () => {
+
+    const filterDrinks = (data, filter) => {
+      return data.filter(drink => drink.type === filter).map(drink => {
+        return (
+          <div className='menu-item'>
+            <Link to={`/menu/${drink.name}`}>
+              <h4>{drink.name}</h4>
+            </Link>
+            <p>{drink.price.split('/')[0]}</p>
+            <p>{drink.price.split('/')[1]}</p>
+          </div>
+        )
+      })
+    }
+
+    // const fruitTea = fruityData.filter(drink => drink.type === 'Fruit Tea').map(drink =>{
+    //   return (
+    //     <div className='menu-item'>
+    //       <Link to={`/menu/${drink.name}`}>
+    //         <h4>{drink.name}</h4>
+    //       </Link>
+    //       <p>{drink.price.split('/')[0]}</p>
+    //       <p>{drink.price.split('/')[1]}</p>
+    //     </div>
+    //   )
+    // })
+
+    const fruitTea = filterDrinks(fruityData, 'Fruit Tea')
+    const yakult = filterDrinks(fruityData, 'Yakult')
+    const milkCap = filterDrinks(fruityData, 'Milk Cap')
+    const milk = filterDrinks(fruityData, 'Milk')
+    const smoothie = filterDrinks(fruityData, 'Smoothie')
+    const milkTea = filterDrinks(fruityData, 'Milk Tea')
+    const signature = filterDrinks(fruityData, 'Signature Smoothie')
+    const specialty = filterDrinks(fruityData, 'Specialty Drink')
+    const freshTea = filterDrinks(fruityData, 'Fresh Brewed Tea')
+
+
+    return (
+      <div className='menu'>
+        <h1>Menu</h1>
+        <h2>FRESH FRUITY ICED TEA</h2>
+        <div className='sizes'>
+          <h5></h5>
+          <p>M</p>
+          <p>L</p>
+        </div>
+        {fruitTea}
+        <h2><span>Yakult</span> DRINK</h2>
+        <div className='sizes'>
+          <h5></h5>
+          <p>M</p>
+          <p>L</p>
+        </div>
+        {yakult}
+        <h2>MILKCAP</h2>
+        <div className='sizes'>
+          <h5></h5>
+          <p>M</p>
+          <p>L</p>
+        </div>
+        {milkCap}
+        <h2>FRESH MILK</h2>
+        <div className='sizes'>
+          <h5></h5>
+          <p>M</p>
+          <p>L</p>
+        </div>
+        {milk}
+        <h2>SLUSH/SMOOTHIE</h2>
+        <div className='sizes'>
+          <h5></h5>
+          <p>M</p>
+          <p>L</p>
+        </div>
+        {smoothie}
+        <h2>MILK TEA</h2>
+        <div className='sizes'>
+          <h5></h5>
+          <p>M</p>
+          <p>L</p>
+        </div>
+        {milkTea}
+        <h2>SIGNATURE SMOOTHIES</h2>
+        <div className='sizes'>
+          <h5></h5>
+          <p>M</p>
+          <p>L</p>
+        </div>
+        {signature}
+        <h2>SPECIALTY DRINK</h2>
+        <div className='sizes'>
+          <h5></h5>
+          <p>M</p>
+          <p>L</p>
+        </div>
+        {specialty}
+        <h2>FRESH BREWED TEA</h2>
+        <div className='sizes'>
+          <h5></h5>
+          <p>M</p>
+          <p>L</p>
+        </div>
+        {freshTea}
+      </div>
+    )
+  }
+
+  const loading = () => {
+    return <h1>Menu Loading...</h1>
+  }
+
+  // return (
+  // <div className='menu'>
+  //   <h1>Menu</h1>
+
+  // </div>
+  // )
+
+  return fruityData ? loaded() : loading()
 }
 
 export default Menu
