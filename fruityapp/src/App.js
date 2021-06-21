3e16993e54fea8fcfc2614591a7a5c4b146eddcb
Chun Hin D. Tang <dtang955472@gmail.com>
2021-06-21 16:23:05 -0400

    added a confirmation when adding drinks to cart

--- fruityapp/src/App.js
+++ fruityapp/src/App.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import './App.scss';
+import "./App.scss";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
@@ -14,53 +14,57 @@ library.add(fas, fab);
 
 function App() {
   // backend api url
-  const url = 'https://fruity-api.herokuapp.com/drinks/'
-
+  const url = "https://fruity-api.herokuapp.com/drinks/";
 
   // fruityData will store the data in drinks data in an array
-  const [fruityData, setFruityData] = useState(null)
+  const [fruityData, setFruityData] = useState(null);
 
   const getData = async () => {
     const resp = await fetch(url);
-    const data = await resp.json()
-    const newData = data.map(drink => {
+    const data = await resp.json();
+    const newData = data.map((drink) => {
       return {
-        "pk": drink.pk,
-        ...drink.fields
-      }
-    })
-    setFruityData(newData)
-  }
+        pk: drink.pk,
+        ...drink.fields,
+      };
+    });
+    setFruityData(newData);
+  };
 
-  useEffect(() => {getData()}, [])
+  useEffect(() => {
+    getData();
+  }, []);
   // console.log(fruityData)
 
-  const [cartList, setCartList] = useState([])
+  const [cartList, setCartList] = useState([]);
 
   const handleAdd = (drink, price, selection) => {
-    setCartList([
-      ...cartList, 
-      {
-        "name": drink.name, 
-        "price": price,
-        ...selection
-      }
-    ])
-  }
+    const confirmAdd = window.confirm("Add to cart?");
+    if (confirmAdd == true) {
+      setCartList([
+        ...cartList,
+        {
+          name: drink.name,
+          price: price,
+          ...selection,
+        },
+      ]);
+    }
+  };
 
   const handleRemove = (index) => {
-    setCartList(cartList.filter((drink, i) => i !== index))
-  }
+    setCartList(cartList.filter((drink, i) => i !== index));
+  };
 
   const handleCheckout = () => {
-    setCartList([])
-    alert("Your order has been recieved! Thank You for Your Patronage!")
-  }
+    setCartList([]);
+    alert("Your order has been recieved! Thank You for Your Patronage!");
+  };
 
   return (
     <div className="App">
-      <Header cartList={cartList}/>
-      <Main 
+      <Header cartList={cartList} />
+      <Main
         fruityData={fruityData}
         cartList={cartList}
         handleAdd={handleAdd}
